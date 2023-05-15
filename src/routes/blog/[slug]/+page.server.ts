47378e3b71f9df8478b1dcb7cf8d764eb4d01837
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-05-15 19:58:27 -0400

    Added dynamic buttons to blog

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,8 +1,10 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import  prisma  from '$lib/server/prisma'
+import {auth} from '$lib/server/lucia'
 
-export const load: PageServerLoad =  (async ({ params }) =>{
+export const load: PageServerLoad =  (async ({ params, locals }) =>{
+    const session = await locals.auth.validate();
     if(params.slug === 'hello-world'){
         return {
             title: 'Hello world',
@@ -14,7 +16,19 @@ export const load: PageServerLoad =  (async ({ params }) =>{
             id: parseInt(params.slug)
         }
     })
-    return{
-        data: post
+    if(await post === null){
+        throw error(404, 'This is not the post you are looking for');
+    }
+    if(await !session){
+        return{
+            data: post,
+            isUser: false
+        }
+    }
+    if(await post.user_id === session.userId){
+        return{
+            data: post,
+            isUser: true
+        }
     }
 });
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,8 +2,12 @@
     import type { PageData } from './$types';
 
     export let data: PageData;
-    console.log(data.data);
 </script>
 
 <h1>{data.data.title}</h1>
 <p>{data.data.content}</p>
+
+{#if data.isUser}
+    <button class="btn variant-filled-warning">Delete</button>
+    <button class="btn variant-filled-primary">Modify</button>
+{/if}
