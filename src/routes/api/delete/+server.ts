3723bed2df8b01cdff64f28cc64b3fd6c31e3e23
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-05-15 20:37:09 -0400

    Delete protected by user verification

--- src/routes/api/delete/+server.ts
+++ src/routes/api/delete/+server.ts
@@ -1,29 +1,39 @@
+import { auth } from "$lib/server/lucia";
 import prisma from "$lib/server/prisma";
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
+import {error} from '@sveltejs/kit'
 
-export const DELETE = (async({ request }) => {
+export const DELETE = (async({ request, locals }) => {
+    const user = await locals.auth.validate();
     const body = await request.json();
     const postId = await body.id
-    //console.log(articleID);
-     async function main(){
-        const post = await prisma.post.delete({
-            where: {
-                id: parseInt(postId),
-            }
+    const userIdRec = await body.user;
+    if(!userIdRec){
+        throw error(400, 'Bad request')
+    }
+    if(userIdRec === user.userId){
+        console.log("success");
+        async function main(){
+            const post = await prisma.post.delete({
+                where: {
+                    id: parseInt(postId),
+                },
+                include: {
+                    auth_user: userIdRec
+                }
+            })
+        }
+        main()
+        .then(async()=>{
+            await prisma.$disconnect()
+        })
+        .catch(async(e)=>{
+            console.error(e)
+            await prisma.$disconnect()
+            process.exit(1)
         })
     }
-    main()
-    .then(async()=>{
-        await prisma.$disconnect()
-        //console.log(await prisma.$disconnect())
-        //console.log(await prisma.$disconnect())
-    })
-    .catch(async(e)=>{
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
     return json({"Post": "Deleted"})
 })satisfies RequestHandler
 
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,12 +2,30 @@
     import type { PageData } from './$types';
 
     export let data: PageData;
+
+    async function deletePost() {
+        try{
+            const req = await fetch('/api/delete',{
+                method: 'DELETE',
+                body: JSON.stringify({
+                    id: data.data.id,
+                    user: data.data.user_id
+                })
+            });
+            const res = await req.json();
+            if(res.Post === "Deleted"){
+                location.assign('/')
+            }
+        }catch(error){
+            console.error(error)
+        }
+    }
 </script>
 
 <h1>{data.data.title}</h1>
 <p>{data.data.content}</p>
 
 {#if data.isUser}
-    <button class="btn variant-filled-warning">Delete</button>
+    <button class="btn variant-filled-warning" on:click={deletePost}>Delete</button>
     <button class="btn variant-filled-primary">Modify</button>
 {/if}
